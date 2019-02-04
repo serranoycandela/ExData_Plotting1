a5e0fa7283f7fffe1ba2bd35d423b60b94cce218
Fidel Serrano <serranoycandela@gmail.com>
2019-02-04 13:10:47 -0600

    geting data from code

--- plot3.R
+++ plot3.R
@@ -1,3 +1,7 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "exdata_data_household_power_consumption.zip", "curl")
+
+unzip("exdata_data_household_power_consumption.zip")
+
 household_power_consumption <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
 
 household_power_consumption$Date <- as.Date(as.character(household_power_consumption$Date), format="%d/%m/%Y")
